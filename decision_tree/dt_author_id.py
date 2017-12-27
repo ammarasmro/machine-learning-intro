ca6ff23a33c3f0a4a757e855d85ca7a0450913fc
ammarasmro <ammarasmaro@gmail.com>
2017-12-27 13:13:52 -0700

    decision trees
    end of lesson 4

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+
 
 
 ### features_train and features_test are the features for the training
@@ -24,8 +25,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
 
+print len(features_train[0])
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+
+print clf.score(features_test, labels_test)
 
 #########################################################
-
-
